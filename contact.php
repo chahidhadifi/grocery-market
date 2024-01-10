b3dade3d409cef50e14919474f2c0962820e964f
elmerjani <mohamedelmerjani3@gamil.com>
2024-01-10 22:53:49 +0100

    add contact.php and category.php files

---
+++ contact.php
@@ -0,0 +1,83 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_POST['send'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $number = $_POST['number'];
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $msg = $_POST['msg'];
+   $msg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+   $select_message = $conn->prepare("SELECT * FROM `message` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+   $select_message->execute([$name, $email, $number, $msg]);
+
+   if($select_message->rowCount() > 0){
+      $message[] = 'Already sent message!';
+   }else{
+
+      $insert_message = $conn->prepare("INSERT INTO `message`(user_id, name, email, number, message) VALUES(?,?,?,?,?)");
+      $insert_message->execute([$user_id, $name, $email, $number, $msg]);
+
+      $message[] = 'Sent message successfully!';
+
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Contact</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="contact">
+
+   <h1 class="title">
+      Contact
+      <div class="underline-title-container">
+         <div class="underline-title"></div>
+      </div>
+   </h1>
+
+   <form action="" method="POST">
+      <input type="text" name="name" class="box" required placeholder="Name">
+      <input type="email" name="email" class="box" required placeholder="Email">
+      <input type="number" name="number" min="0" class="box" required placeholder="Number">
+      <textarea name="msg" class="box" required placeholder="Message" cols="30" rows="10"></textarea>
+      <input type="submit" value="Send message" class="btn" name="send">
+   </form>
+
+</section>
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
